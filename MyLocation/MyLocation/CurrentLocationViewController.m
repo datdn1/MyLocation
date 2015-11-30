339bba352e451e045cf57509dc491404ae8a3150
datdn1 <datdn1.27@gmail.com>
2015-11-30 11:19:02 +0700

    Add corelocation and get location

--- MyLocation/MyLocation/CurrentLocationViewController.m
+++ MyLocation/MyLocation/CurrentLocationViewController.m
@@ -7,12 +7,25 @@
 //
 
 #import "CurrentLocationViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface CurrentLocationViewController ()
 
+@interface CurrentLocationViewController () <CLLocationManagerDelegate>
 @end
 
 @implementation CurrentLocationViewController
+{
+    CLLocationManager *_coreLocationManager;
+}
+// create location manager when load from storyboard
+-(instancetype) initWithCoder:(NSCoder *)aDecoder {
+    if(self = [super initWithCoder:aDecoder]) {
+        _coreLocationManager = [[CLLocationManager alloc] init];
+    }
+    return self;
+}
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,7 +37,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+// implement delegate of core location to get current location
+// turn on GPS when need fix location
 - (IBAction)getLocation:(id)sender {
+    _coreLocationManager.delegate = self;
+    _coreLocationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    // enable core location by code
+    [_coreLocationManager requestWhenInUseAuthorization];
+    [_coreLocationManager requestAlwaysAuthorization];
+    
+    [_coreLocationManager startUpdatingLocation];
+}
+
+#pragma mark - CoreLocation delegates
+-(void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"didFailWithError: %@", error);
+}
+
+-(void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    CLLocation *newLocation = [locations lastObject];
+    NSLog(@"New location: %@", newLocation);
 }
 
 
